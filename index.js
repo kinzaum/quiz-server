ebbcecfb32dd21bf5a8c25c63cb519c8e15bf47d
Fabio Dias <kinzaum@gmail.com>
2025-09-07 09:35:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-﻿const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const io = require("socket.io")(PORT, {
     cors: { origin: "*", methods: ["GET", "POST"] },
     transports: ["websocket"]
@@ -9,12 +9,12 @@ console.log(`📡 Quiz server running on port ${PORT}...`);
 io.on("connection", (socket) => {
     console.log("🔗 New client connected: " + socket.id);
 
-    socket.on("newQuestion", (question) => {
-        socket.broadcast.emit("newQuestion", question);
-    });
-
-    socket.on("newAnswer", (answer) => {
-        io.emit("newAnswer", answer);
+    // Listen for a "buttonPressed" event from a client
+    socket.on("buttonPressed", () => {
+        // Broadcast a "buttonPressed" message to all other connected clients
+        // This is the message that will trigger the text update on the other device.
+        socket.broadcast.emit("buttonPressed");
+        console.log(`➡️ Button press event broadcasted from client: ${socket.id}`);
     });
 
     socket.on("disconnect", () => {
